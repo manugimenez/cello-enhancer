a8ff451d9f6e5e94dce3ca957cc815b43b6b0007
CeilingFANC <cf10293@163.com>
2017-03-23 22:40:34 -0400

    MEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

---
+++ algorithms.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 23 22:27:38 2017
+
+@author: Mentat
+"""
+
+potential_gates={}
+potential_gates['gate_name']={'ymax':0,'ymin':0,'K':0,'n':0}
+potential_gates['gate_name']={'ymax':0,'ymin':0,'K':0,'n':0}
+
+chain=[]
+chain.append('name of the gate being used,first gate')
+chain.append('name of the gate being used,second gate')
+chain.append('name of the gate being used,third gate')
+
+
+inputs=[0,1,2,3]
+#the inputs on x asxis, from left to right, 
+#
+
+result_name_chain=[]
+result_name_chain.append('name of the gate being used,first gate')
+result_name_chain.append('name of the gate being used,second gate')
+result_name_chain.append('name of the gate being used,third gate')
+
+
+
+
+def design(potential_gates,chain,inputs):
+    
+    
+    
+    return result_name_chain
+    #the changed parameter is saved in the potential_gates
+    #score would be good too
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
